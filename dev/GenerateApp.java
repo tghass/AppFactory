e2897cce3aed5683509d5cd59b0df491ea6b14f5
Adam Kafka <adkafka@gmail.com>
2015-10-22 14:43:29 -0400

    Moved all SqlCode generation to one file. Cleaned some small things up.

--- dev/GenerateApp.java
+++ dev/GenerateApp.java
@@ -5,29 +5,15 @@ import java.util.*;
 //First, parses the config file file
 public class GenerateApp {
 
-	public static String toSql(HashMap<String,DataObj> dataObjsMap){
-        StringBuilder s = new StringBuilder(4096);
-        s.append("CREATE DATABASE  IF NOT EXISTS `").append("DBNAME").append("`;\n");
-        s.append("USE `").append("DBNAME").append("`;\n");
-        Iterator it = dataObjsMap.entrySet().iterator();
-        while(it.hasNext()){
-            Map.Entry one = (Map.Entry)it.next();
-            s.append(((DataObj)one.getValue()).toSql());
-        }
-        return s.toString();
-    }
-	
-	
-	
 	public static void main(String [] args) {
-		String filename = args[0]; //TODO: accept file from command line
+		String filename = args[0];
 		Parser parser = new Parser();
 
 		//Parse file: convert JavaScript objects to Java objects (DataObjects class) 
 		parser.parseFile(filename); 
 		
 		//From the DataObjects
-		String sqldump = toSql(parser.dataObjsMap); //generate sqldump file
+		String sqldump = SqlGenerator.toSql(parser.getDataObjsMap()); //generate sqldump file
 		
 		System.out.println(sqldump);
 	}
