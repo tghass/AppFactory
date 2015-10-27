1d94d1632d8908938c06baefac2e415b9c89fd33
Tamara Hass <tgh216@lehigh.edu>
2015-10-27 12:48:20 -0700

    Hopefully corrected ApiCreator

--- dev/ApiCreator.java
+++ dev/ApiCreator.java
@@ -1,5 +1,7 @@
+
 import java.util.*;
-<<<<<<< HEAD
+
+//Generates API following CRUD operations
 
 public class ApiCreator {
 
@@ -11,22 +13,11 @@ public class ApiCreator {
         StringBuilder s = new StringBuilder(4096); //TODO: change this #
        
         s.append(genDbConnection());
-=======
-public class ApiCreator {
-
-    public static int tabDepth = 0;
-
-    public static String createServerFile(HashMap<String,DataObj> dataObjsMap){
-        StringBuilder s = new StringBuilder(4096); //TODO: change this #
-        String db = genDbConnection();
-        s.append(s);
->>>>>>> c95c8dff9358acf77a5ce6915db17d9e38541983
         //For each data object, get object by ID
         Iterator it = dataObjsMap.entrySet().iterator();
         while(it.hasNext()){
             Map.Entry one = (Map.Entry)it.next();
             s.append(genGetById((DataObj)one.getValue()));
-<<<<<<< HEAD
         }
 		s.append(genListen());
 		System.out.println(s.toString());
@@ -52,23 +43,6 @@ public class ApiCreator {
 		
         //TODO: replace with generated host,user,password, info
         s.append("var mysql = require('mysql');\n");
-=======
-            List<DataObj> foreignKeyObjs = ((DataObj)one.getValue()).getDependencies();
-            if (!foreignKeyObjs.isEmpty()) {
-                s.append(genGetByFK(((DataObj)one.getValue())));
-            }
-        }
-        return s.toString();
-    }
-
-    public static String genDbConnection() {
-        StringBuilder s = new StringBuilder(4096); //TODO: change this #
-        s.append("//Express is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications\n");
-        s.append("var express = require('express');\n");
-        s.append("var app = express();\n");
-        s.append("\n//Establish connection to the MySQL database\n");
-        //TODO: replace with generated host,user,password, info
->>>>>>> c95c8dff9358acf77a5ce6915db17d9e38541983
         s.append("var con = mysql.createConnection({\n");
         s.append("\thost : 'localhost',\n");
         s.append("\tuser : 'root',\n");
@@ -85,28 +59,12 @@ public class ApiCreator {
         s.append("\tconsole.log('Connection established.');\n");
         s.append("\t});\n");
         s.append("\n");
-<<<<<<< HEAD
 		
 		
         return s.toString();
     }
 
     
-=======
-        return s.toString();
-    }
-
-    //What is this supposed to do?
-    public static String genGetByFK(DataObj dataObj) {
-        for (Field f: dataObj.getFields()) {
-            if (f.getType() == Field.Type.FOREIGN_KEY) {
-                System.out.println(f.getName() + f.getTypeStr());
-            }
-        }
-
-        return "";
-    }
->>>>>>> c95c8dff9358acf77a5ce6915db17d9e38541983
     //Generate a get by id call for every table.
     //For the tables with foreign keys, this calls function
     // genGetByForeignKey 
@@ -131,7 +89,6 @@ public class ApiCreator {
             }
         }
         s.append("app.get('/" + tableName + "/find/:id', function(req,res) {\n");
-<<<<<<< HEAD
 		
 		//generates the BASIC select * from table where id = ?
         s.append("\tvar query = \""+ selectProperties(dataObj) + 
@@ -159,27 +116,6 @@ public class ApiCreator {
     }
 
 	//generates the number of tabs the line needs
-=======
-        s.append("\tvar query = \""+ selectProperties(dataObj) + " from " + tableName + " as " +tableName.charAt(0)+ " where id = ?\";\n");
-        s.append("\tcon.query(query, req.params.id, function(err, rows0,fields) {\n");
-        s.append("\t\tif(err) throw err;\n");
-
-        s.append(evaluateFK(dataObj,0,0,1, "", ""));
-
-        s.append(returnTab(tabDepth+4) + "res.jsonp(rows0);\n");
-        while (tabDepth +4> 0) {
-            s.append(returnTab(3+tabDepth) + "});\n");
-            tabDepth -= 1;
-        }
-        System.out.println(s.toString());
-
-        s.append("\n");
-
-
-        return s.toString();
-    }
-
->>>>>>> c95c8dff9358acf77a5ce6915db17d9e38541983
     public static String returnTab(int depth) {
         StringBuilder s = new StringBuilder(128);
         for (int i = 0; i < depth; i++) {
@@ -188,33 +124,19 @@ public class ApiCreator {
         return s.toString();
     }
 
-<<<<<<< HEAD
 	//generates the 'select' part of the SQL select statement
     public static String selectProperties(DataObj d) {
         StringBuilder s = new StringBuilder(1024);
         String name = d.getName();
-=======
-    public static String selectProperties(DataObj d) {
-        StringBuilder s = new StringBuilder(1024);
-        String name = d.getName();
-        char firstChar = d.getName().charAt(0);
->>>>>>> c95c8dff9358acf77a5ce6915db17d9e38541983
         s.append("select ");
         boolean firstField = true;
         for (Field f: d.getFields()) {
             if (firstField) {
                 firstField = false;
-<<<<<<< HEAD
                 s.append(name + "." + f.getName() +" ");
             }
             else {
                 s.append(", " + name + "." + f.getName() +" ");
-=======
-                s.append(firstChar + "." + f.getName() +" ");
-            }
-            else {
-                s.append(", " + firstChar + "." + f.getName() +" ");
->>>>>>> c95c8dff9358acf77a5ce6915db17d9e38541983
             }
         }
         return s.toString();
@@ -223,7 +145,6 @@ public class ApiCreator {
     //query NO starts at 0
     //queryNoNext starts at 1
     //depth starts at 0
-<<<<<<< HEAD
 	// DataObj d - data object that's foreign keys are being expanded from ids to JSON objects
 	// depth - tab placement
 	// queryNo - which query is currently 
@@ -232,11 +153,6 @@ public class ApiCreator {
         HashMap<DataObj, List<String>> dataObjsMap = new HashMap<DataObj, List<String>>(); 
 		
         //First, evaluate all the foreign keys on the current object
-=======
-    public static String evaluateFK(DataObj d, int depth, int queryNo, int queryNoNext, String lastQueryFrom, String lastQueryWhere) {
-        StringBuilder s = new StringBuilder(4028);
-        HashMap<DataObj, List<String>> dataObjsMap = new HashMap<DataObj, List<String>>(); 
->>>>>>> c95c8dff9358acf77a5ce6915db17d9e38541983
         for (Field f : d.getFields()) {
             if (f.getType() == Field.Type.FOREIGN_KEY) {	
                 // get the object corresponding to the foreign key field str
@@ -246,7 +162,6 @@ public class ApiCreator {
                         String parentRow = "rows" + Integer.toString(queryNo);
                         String fkRow = "rows" + Integer.toString(queryNoNext);
                         String fkName = fkObj.getName();
-<<<<<<< HEAD
                         String parName = d.getName();
 						
 						//The 'from' part of a SQL select FROM where
@@ -300,41 +215,10 @@ public class ApiCreator {
 		//						 [1] - queryWhere
 		//						 [2] - queryNo 
 		// The dataObjsMap is populated roughly 10 lines up
-=======
-                        char fkChar = fkName.charAt(0);//What if there are two foreign keys with the same first letter? How about we use the whole fkName?
-                        String parName = d.getName();
-                        char parChar = parName.charAt(0);
-                        String queryFrom = lastQueryFrom + " " + fkName + " as " + fkChar + " ";
-                        String queryWhere = lastQueryWhere + " " +  fkChar + ".id = " + parChar + "." + f.getName();
-                        if (queryNo == 0) {
-                            queryFrom += " inner join " + parName + " as " + parChar + " "; 
-                            queryWhere += " and " + parChar + ".id = ?";
-                        }
-                        s.append(returnTab(2+depth) + parentRow + ".forEach(function(row) { \n");
-                        s.append(returnTab(3+depth) + "query = \""+ selectProperties(fkObj) + "\";\n");
-                        s.append(returnTab(3+depth) + "query += \"from "+ queryFrom + "\";\n");
-                        s.append(returnTab(3+depth) + "query += \"where "+ queryWhere + "\";\n");
-                        s.append(returnTab(3+depth) + "con.query(query, req.params.id, function(err, " + fkRow+",fields) {\n");
-                        s.append(returnTab(4+depth) + "if (err) throw err;\n");
-                        s.append(returnTab(4+depth) + "row." + f.getName()+ " = " + fkRow+"[0];\n");
-
-                        dataObjsMap.put(fkObj, Arrays.asList(queryFrom + " inner join ", queryWhere + " and ", Integer.toString(queryNoNext) )); 
-                        depth+=2;
-                        queryNoNext++;
-
-                    }
-                }
-
-                //first, evaluate all the foreign keys on the current object
-
-            }
-        }
->>>>>>> c95c8dff9358acf77a5ce6915db17d9e38541983
         Iterator it = dataObjsMap.entrySet().iterator();
         while(it.hasNext()){
             Map.Entry one = (Map.Entry)it.next();
             DataObj key = (DataObj)one.getKey();
-<<<<<<< HEAD
 			
             List<String> values = (List<String>)dataObjsMap.get(key);
 			
@@ -346,21 +230,6 @@ public class ApiCreator {
 			s.append(evaluateFK(key,depth,(String)values.get(0), (String)values.get(1)));
         }
         if (depth > tabDepth) {tabDepth = depth;}
-=======
-            //UNSAFE OPERATIONS HERE, unchecked cast
-            List<String> values = (List<String>)one.getValue();//What is this and why are we parsing ints? This is not obvious
-            s.append(evaluateFK(
-                        (key),
-                        depth,
-                        Integer.parseInt(values.get(2)),
-                        queryNoNext,
-                        values.get(0),
-                        values.get(1))
-                    );
-        }
-
-        tabDepth = depth;
->>>>>>> c95c8dff9358acf77a5ce6915db17d9e38541983
         return s.toString();
     }
 }
