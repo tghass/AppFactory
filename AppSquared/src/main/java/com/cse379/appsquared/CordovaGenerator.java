ac41b62ac3b42296c926c19b8f5e1bdd5cb97c1b
Adam Kafka <adkafka@gmail.com>
2015-11-04 20:48:00 -0500

    Fixed not making the package. Now CG cpoies all resources over to the cordova dir

--- AppSquared/src/main/java/com/cse379/appsquared/CordovaGenerator.java
+++ AppSquared/src/main/java/com/cse379/appsquared/CordovaGenerator.java
@@ -1,12 +1,13 @@
 package com.cse379.appsquared;
 
 import java.io.*;
+import org.apache.commons.io.FileUtils;
 public class CordovaGenerator{
 
     //////////
     //Fields//
     //////////
-    public static final String REF_DIR = "Reference/www";
+    public static final File REF_DIR = new File("resources/www");
     
     private File outputDir;
 
@@ -16,9 +17,16 @@ public class CordovaGenerator{
 
     /** Constructor for CordovaGenerator */
     public CordovaGenerator(File out){
+        System.out.println("HERE!!!");
         outputDir=out;
         outputDir.mkdir();//Make the directory
         //Copy over the reference directory
+        try{
+            System.out.println(REF_DIR.exists());
+            FileUtils.copyDirectory(REF_DIR,outputDir);
+        }catch (IOException e){
+            System.out.println("Error copying over reference dir for cordova");
+        }
     }
 
     ///////////
